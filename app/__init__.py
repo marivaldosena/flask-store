4d84ab2610157bd74325b3f7e04317e331bcc558
Marivaldo Sena <msena.ifsp@gmail.com>
2019-04-07 17:38:58 -0300

    Criar as funções e os endpoints para a criação de lojas e itens.

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,40 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
-def home():
-    return 'Hello, world!'
+stores = [
+    {
+        'name': 'Store 1',
+        'items':
+        [
+            {
+                'name': 'Item 1',
+                'price': 15.99
+            }
+        ]
+    }
+]
+
+
+@app.route('/store', methods=['POST'])
+def create_store():
+    pass
+
+
+@app.route('/store/<string:name>')
+def get_store(name):
+    pass
+
+
+@app.route('/store')
+def get_all_stores():
+    pass
+
+
+@app.route('/store/<string:name>/item', methods=['POST'])
+def create_item_in_store(name):
+    pass
+
+
+@app.route('/store/<string:name>/item')
+def get_items_in_store(name):
+    pass
