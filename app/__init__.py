e9980cb15266b4b4c4d64ac572a65c2ec6e90caa
Marivaldo Sena <msena.ifsp@gmail.com>
2019-04-09 19:13:18 -0300

    Criar Resource para o Item de loja. Implementar endpoints para criação e obtenção de itens.

--- app/__init__.py
+++ app/__init__.py
@@ -17,7 +17,8 @@ stores = [
 ]
 
 parser = reqparse.RequestParser()
-parser.add_argument('name', required=True, type=str, help='Name é obrigatório.')
+parser.add_argument('name', required=True, type=str,
+                    help='Name é obrigatório.')
 
 
 class StoreListResource(Resource):
@@ -45,37 +46,36 @@ class StoreResource(Resource):
         return jsonify(new_store)
 
 
+class StoreItemResource(Resource):
+    def get(self, name):
+        items = [store['items'] for store in stores if store['name'] == name]
+
+        if items:
+            return jsonify({'itens': items})
+        else:
+            return jsonify({'message': 'Store not found'})
+
+    def post(self, name):
+        json = request.get_json()
+
+        new_item = {
+            'name': json.get('name', None),
+            'price': json.get('price', None)
+        }
+
+        for store in stores:
+            if store['name'] == name:
+                store['items'].append(new_item)
+                return jsonify(store)
+
+        return jsonify({'message': 'Store not found'})
+
+
 api.add_resource(StoreListResource, '/store', '/stores')
-api.add_resource(StoreResource, '/store','/store/<string:name>')
+api.add_resource(StoreResource, '/store', '/store/<string:name>')
+api.add_resource(StoreItemResource, '/store/<string:name>/item')
 
 
 @app.route('/')
 def home():
     return render_template('index.html')
-
-
-@app.route('/store/<string:name>/item', methods=['POST'])
-def create_item_in_store(name):
-    json = request.get_json()
-
-    new_item = {
-        'name': json.get('name', None),
-        'price': json.get('price', None)
-    }
-
-    for store in stores:
-        if store['name'] == name:
-            store['items'].append(new_item)
-            return jsonify(store)
-
-    return jsonify({'message': 'Store not found'})
-
-
-@app.route('/store/<string:name>/item')
-def get_items_in_store(name):
-    items = [store['items'] for store in stores if store['name'] == name]
-
-    if items:
-        return jsonify({'itens': items})
-    else:
-        return jsonify({'message': 'Store not found'})
